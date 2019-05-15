de11edc4468c853deecdc21ad326e12ca425cae7
Shai Nagar <shain@wix.com>
2019-05-15 16:49:20 +0300

    formatting

--- inspector/util/console.py
+++ inspector/util/console.py
@@ -1,12 +1,14 @@
-
 def info(text):
-  print("\033[0;37;40mINFO: %s" % text)
+    print("\033[0;37;40mINFO: %s" % text)
+
 
 def success(text):
-  print("\033[1;32;40mINFO: %s" % text)  
+    print("\033[1;32;40mINFO: %s" % text)
+
 
 def warn(text):
-  print("\033[1;33;40mWARN: %s" % text)
+    print("\033[1;33;40mWARN: %s" % text)
+
 
 def error(text):
-  print("\033[1;31;40mERRO: %s" % text)
+    print("\033[1;31;40mERRO: %s" % text)
