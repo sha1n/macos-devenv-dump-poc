d600acb4c60883bfc6aef3e541adfb1e7dfa9906
Shai Nagar <shain@wix.com>
2019-05-16 07:56:08 +0300

    nicer console messages

--- inspector/util/console.py
+++ inspector/util/console.py
@@ -1,14 +1,18 @@
 def info(text):
-    print("\033[0;37;40mINFO: %s" % text)
-
-
-def success(text):
-    print("\033[1;32;40mINFO: %s" % text)
+    print("\033[1;37;40mINFO\033[0;0m: %s" % text)
 
 
 def warn(text):
-    print("\033[1;33;40mWARN: %s" % text)
+    print("\033[1;33;40mWARN\033[0;0m: %s" % text)
 
 
 def error(text):
-    print("\033[1;31;40mERRO: %s" % text)
+    print("\033[1;31;40mERRO\033[0;0m: %s" % text)
+
+
+def success(text):
+    print("\033[1;37;40mINFO: \033[0;30;42m%s\033[0;0m" % text)
+
+
+def failure(text):
+    print("\033[1;31;40mERRO: \033[0;37;41m%s\033[0;0m" % text)
