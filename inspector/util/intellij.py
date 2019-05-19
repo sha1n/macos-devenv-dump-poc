ff4f77b2d4bf1fbe33ad5c68db6237c002083402
Shai Nagar <shain@wix.com>
2019-05-19 09:49:18 +0300

    filtering out log files older than two weeks

--- inspector/util/intellij.py
+++ inspector/util/intellij.py
@@ -4,6 +4,9 @@ from shutil import copyfile
 from shutil import copytree
 import os
 import json
+import time
+
+two_week_sec = 14 * 24 * 60 * 60  # days * hours * minutes * seconds
 
 
 def collect_intellij_info_files(user_home_dir_path, target_dir_path):
@@ -13,11 +16,15 @@ def collect_intellij_info_files(user_home_dir_path, target_dir_path):
         code = _read_json_property(file_path, "productCode")
         copyfile(file_path, "%s/intellij-%s-%s-product-info.json" % (target_dir_path, code, version))
 
-    log.info("\t- Collecting logs...")
+    log.info("\t- Collecting logs... (files older than two weeks will be ignored)")
     for logs_dir_path in _collect_log_libraries(user_home_dir_path):
         dir_name = _file_name_from(logs_dir_path)
 
-        copytree(logs_dir_path, "%s/logs/%s" % (target_dir_path, dir_name))
+        copytree(
+            src=logs_dir_path,
+            dst="%s/logs/%s" % (target_dir_path, dir_name),
+            ignore=_ignore_files_mtime_gt(two_week_sec)
+        )
 
     log.info("\t- Collecting configuration files...")
     for config_dir_path in _collect_configurations(user_home_dir_path):
@@ -56,3 +63,13 @@ def _read_json_property(file_path, property_name):
     with open(file_path) as json_file:
         data = json.load(json_file)
         return data[property_name]
+
+
+def _ignore_files_mtime_gt(interval_sec):
+    def ignore(path, names):
+        return filter(
+            lambda name: os.path.getmtime("%s/%s" % (path, name)) < time.time() - interval_sec,
+            names
+        )
+
+    return ignore
