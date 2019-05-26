64c72df686fcccc2a6f1e3e14bdd961a5c489c5f
Shai Nagar <shain@wix.com>
2019-05-26 15:48:49 +0300

    fixed utility tests script

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,6 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-PYTHONPATH=${SCRIPT_DIR}/pkg python3 -m unittest discover -p "*_test.py"
+cd ${SCRIPT_DIR}/pkg
+
+python3 -m unittest discover -p "*_test.py"
